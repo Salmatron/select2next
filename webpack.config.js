f99470c6dbb6375d5b7e7486d177943108da9979
Salman Abbas <salmanabbas007@gmail.com>
2018-10-18 06:57:30 +0500

    Added WebPack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/jquery.select2.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'select2.js'
+    },
+    //mode: 'development',
+    devtool: 'source-map',
+};
